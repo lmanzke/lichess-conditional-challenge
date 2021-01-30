131a061122a5416b6c100434bbd96cefa51f1eb7
Lucas Manzke <lucas.manzke@cloud.ionos.com>
2021-01-30 17:47:18 +0100

    Small refactorings

--- src/challenge/App.vue
+++ src/challenge/App.vue
@@ -19,7 +19,6 @@ import {
 import { getLichessPrefs } from '@/challenge/storage';
 import { defineComponent, provide } from 'vue';
 import { JpexInstance } from 'jpex';
-import * as E from 'fp-ts/Either';
 import * as TE from 'fp-ts/TaskEither';
 import * as RT from 'fp-ts/ReaderTask';
 import * as RTE from 'fp-ts/ReaderTaskEither';
@@ -27,7 +26,7 @@ import * as IO from 'fp-ts/IO';
 import { flow, pipe } from 'fp-ts/function';
 import { sequenceT } from 'fp-ts/Apply';
 import { array } from 'fp-ts/Array';
-import { fromArray, head, NonEmptyArray } from 'fp-ts/NonEmptyArray';
+import { fromArray, head } from 'fp-ts/NonEmptyArray';
 
 interface Props {
   container: HTMLElement;
@@ -59,28 +58,21 @@ const retrieveChallengeTask = (challengeElement: HTMLDivElement, retrieveChallen
     reason => Error(String(reason))
   );
 
-const processChallenges: (
-  args: [Spec, Challenge[]]
-) => RTE.ReaderTaskEither<
-  { retrieveChallenge: ChallengeRetriever; convertRule: RuleConverter; challengeProcessorFactory: (spec: Spec) => ChallengeProcessor },
-  Error,
-  { challenge: Challenge; silent: boolean }[]
-> = ([spec, challenges]) => r => {
+const processChallenges: (args: [Spec, Challenge[]]) => RTE.ReaderTaskEither<ReaderDeps, Error, ChallengeInfo[]> = ([spec, challenges]) => r => {
   return TE.fromTask(() => {
     const challengeProcessor = r.challengeProcessorFactory(spec);
     return challengeProcessor(challenges, []);
   });
 };
 
-const retrieveData = (
-  challengeElement: HTMLDivElement
-): RTE.ReaderTaskEither<{ retrieveChallenge: ChallengeRetriever; convertRule: RuleConverter }, Error, [Spec, Challenge[]]> => r =>
+const retrieveData = (challengeElement: HTMLDivElement): RTE.ReaderTaskEither<ReaderDeps, Error, [Spec, Challenge[]]> => r =>
   sequenceTaskEither(lichessPrefTask(r.convertRule), retrieveChallengeTask(challengeElement, r.retrieveChallenge));
 
 const ensureChallenge = (challengeInfo: ChallengeInfo[]) => {
-  const nea = fromArray(challengeInfo);
-  const neaEither = E.fromOption(() => Error('No matching challenge'))(nea);
-  return RTE.fromEither<ReaderDeps, Error, NonEmptyArray<ChallengeInfo>>(neaEither);
+  return pipe(
+    RTE.of<ReaderDeps, Error, ChallengeInfo[]>(challengeInfo),
+    RTE.chain(v => RTE.fromOption(() => Error('No matching challenge'))(fromArray(v)))
+  );
 };
 
 const acceptFirstChallenge = flow(
@@ -94,7 +86,7 @@ const declineAllChallenges = RTE.chain<ReaderDeps, Error, ChallengeInfo[], void>
     challengeInfos,
     v => v.map(challengeInfo => challengeInfo.challenge.decline),
     array.sequence(IO.io),
-    IO.map(() => {}),
+    IO.map(challenges => `Declined ${challenges.length} challenges`),
     v => RTE.fromIO<ReaderDeps, Error, void>(v)
   )
 );
